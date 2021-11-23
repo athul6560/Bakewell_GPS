c286070729ac547e6fb8acce1223440943cee756
athul <athul.6560@gmail.com>
2021-11-24 01:37:13 +0530

    imaeg capture done

--- app/src/main/java/com/acb/bakewellgps/ui/Activities/EditPage/EditActivity.java
+++ app/src/main/java/com/acb/bakewellgps/ui/Activities/EditPage/EditActivity.java
@@ -1,16 +1,83 @@
 package com.acb.bakewellgps.ui.Activities.EditPage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Toast;
 
 import com.acb.bakewellgps.R;
+import com.acb.bakewellgps.databinding.ActivityDashboardBinding;
+import com.acb.bakewellgps.databinding.ActivityEditBinding;
 
 public class EditActivity extends AppCompatActivity {
+private ActivityEditBinding binding;
+    private static final int CAMERA_REQUEST = 1888;
 
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit);
+        binding = ActivityEditBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initToolsbar();
+        binding.addimageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                }
+                else
+                {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            binding.shopImage.setImageBitmap(photo);
+        }
+    }
+    private void initToolsbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(Html.fromHtml("Edit Shop"));
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 }
